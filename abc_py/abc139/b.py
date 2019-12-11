fcbe64188206a8057d9f88b4f70c8cf239341b68
aki-nasu <ayokaro.inkyushu@gmail.com>
2019-12-11 16:38:59 +0900

    ac

--- abc_py/abc139/b.py
+++ abc_py/abc139/b.py
@@ -1,7 +1,9 @@
 # 12/11 15:50 give up
+# 高橋くんの家には電源プラグの差込口が1口しかありません。
+# 上記のパターンを見落としていた。1口で十分な場合、電源タップは0個で良い。
 a,b = map(int,input().split())
-ans = 1
-t = a
+ans = 0
+t = 1
 while True:
     if t >= b:
         print(ans)
