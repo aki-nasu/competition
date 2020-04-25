fbfd5f6f6104a3e887ed25777e5a907d34d0b6e1
aki-nasu <t.t.akiyoshi2@gmail.com>
2020-04-25 22:28:14 +0900

    streak

--- abc_py/abc085/b.py
+++ abc_py/abc085/b.py
@@ -1,18 +1,3 @@
 n = int(input())
-d = []
-for _ in range(n):
-    d.append(int(input()))
-count_0 = 1
-d = sorted(d)
-for i in range(n):
-    if i == n-1: break
-    if d[i] == d[i+1]: continue
-    count_0 += 1
-
-d = sorted(d,reverse=True)
-count_1 = 1
-for i in range(n):
-    if i == n-1: break
-    if d[i] == d[i+1]: continue
-    count_1 += 1
-print(max(count_0,count_1))
+d = set([int(input()) for _ in range(n)])
+print(len(d))
